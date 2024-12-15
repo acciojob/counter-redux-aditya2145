6e263fef29debe79cc9cce86199122362e99d7ca
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-12-15 19:46:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
 import React from "react";
-import './../styles/App.css';
-
+import { useDispatch,useSelector } from "react-redux";
+import {incFn,decFn} from '../redux/counterSlice'
 const App = () => {
+  const counter = useSelector(state => state.counter);
+  const dispatch  = useDispatch();
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>{counter}</h1>
+       <button onClick={() => dispatch(incFn())}>increment</button>
+       
+       <button onClick={() => dispatch(decFn())}>decrement</button>
     </div>
   )
 }
